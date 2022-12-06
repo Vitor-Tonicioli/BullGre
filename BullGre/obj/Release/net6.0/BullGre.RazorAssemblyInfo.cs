8f2c5c97e9d0cdfae0c2097c23310a7a54531a71
Marco <marcoalcindogitti@gmail.com>
2022-12-06 10:40:30 -0300

    Listagem e cadastro de fazendas

--- BullGre/obj/Release/net6.0/BullGre.RazorAssemblyInfo.cs
+++ BullGre/obj/Release/net6.0/BullGre.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BullGre")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("BullGre")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BullGre")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
